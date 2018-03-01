3668b822efde69d4c7267310616854818dfc14bc
Ari Kramer <arikramer24@gmail.com>
2018-03-01 15:11:17 -0500

    adjusted swiper height

--- Client/Components/Toilet.js
+++ Client/Components/Toilet.js
@@ -1,34 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, Image, Dimensions } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Swiper from 'react-native-swiper';
+
+const { width, height } = Dimensions.get('window');
 
 const Toilet = (props) => {
   const { name, rating, unisex, stallsMen, stallsWomen, urinals, changingTable, needBuy } = props.navigation.state.params;
   return (
     <View style={styles.container}>
-      <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Image
-            source={require('../../toilet.jpg')}
-            style={styles.image}
-          />
-        </View>
-        <View style={styles.slide2}>
-          <Image
-            source={require('../../babypee.jpg')}
-            style={styles.image}
-          />
-        </View>
-      </Swiper>
+      {console.log(width, height, 'hi')}
+      <View height={height / 2}>
+        <Swiper style={styles.wrapper}>
+          <View style={styles.slide}>
+            <Image
+              source={require('../../toilet.jpg')}
+              style={styles.image}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('../../babypee.jpg')}
+              style={styles.image}
+            />
+          </View>
+        </Swiper>
+      </View>
       <View style={styles.amenities}>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Rating: </Text>{rating}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Unisex: </Text>{unisex ? 'Yes' : 'No'}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Men's stalls: </Text>{stallsMen}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Urinals: </Text>{urinals}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Women's stalls: </Text>{stallsWomen}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Changing Table: </Text>{changingTable ? 'Yes' : 'No'}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Need to pay: </Text>{needBuy ? 'Yes' : 'No'}</Text>
+        <View style={styles.headerContainer}>
+          <Text style={styles.bathroomHeader}>{name}</Text>
+          <Text style={styles.amenitiesText}><Text style={styles.amenity}>Rating: </Text>{rating}</Text>
+        </View>
+        <View style={styles.amenitiesContainer}>
+          <View style={styles.amenitiesColumn}>
+            <Text style={styles.amenitiesText}><Text style={styles.amenity}>Unisex: </Text>{unisex ? 'Yes' : 'No'}</Text>
+            <Text style={styles.amenitiesText}><Text style={styles.amenity}>Men's stalls: </Text>{stallsMen}</Text>
+            <Text style={styles.amenitiesText}><Text style={styles.amenity}>Urinals: </Text>{urinals}</Text>
+          </View>
+          <View style={styles.amenitiesColumn}>
+            <Text style={styles.amenitiesText}><Text style={styles.amenity}>Women's stalls: </Text>{stallsWomen}</Text>
+            <Text style={styles.amenitiesText}><Text style={styles.amenity}>Changing Table: </Text>{changingTable ? 'Yes' : 'No'}</Text>
+            <Text style={styles.amenitiesText}><Text style={styles.amenity}>Need to pay: </Text>{needBuy ? 'Yes' : 'No'}</Text>
+          </View>
+        </View>
       </View>
     </View>
   )
@@ -40,38 +54,40 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1
   },
+  headerContainer: {
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  amenitiesContainer: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
   amenity: {
     fontWeight: 'bold'
   },
   amenities: {
     display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    marginTop: 10,
+    marginBottom: 10
   },
   amenitiesText: {
     marginLeft: 10,
     fontSize: 20
   },
+  amenitiesColumn: {
+    width: 150
+  },
+  bathroomHeader: {
+    fontSize: 40
+  },
   image: {
-    width: 400,
-    height: 400
+    height: height / 2,
+    width: 400
   },
   wrapper: {
-    // marginTop: -65
   },
   slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 400
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 400
-  },
-  slide3: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
