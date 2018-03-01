b4e3df633faae9b90d0b53cf7aaeec5da34c41ad
Ari Kramer <arikramer24@gmail.com>
2018-03-01 11:01:44 -0500

    added image swiper to toilet page

--- Client/Components/Toilet.js
+++ Client/Components/Toilet.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import Swiper from 'react-native-swiper';
 
 const Toilet = (props) => {
   const { name, rating, unisex, stallsMen, stallsWomen, urinals, changingTable, needBuy } = props.navigation.state.params;
   return (
     <View style={styles.container}>
-      <Image
-      source={require('../../toilet.jpg')}
-      style={styles.image}
-      />
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Image
+            source={require('../../toilet.jpg')}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.slide2}>
+          <Image
+            source={require('../../babypee.jpg')}
+            style={styles.image}
+          />
+        </View>
+      </Swiper>
       <View style={styles.amenities}>
         <Text style={styles.amenitiesText}><Text style={styles.amenity}>Rating: </Text>{rating}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Unisex: </Text>{unisex ? 'Yes': 'No'}</Text>
+        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Unisex: </Text>{unisex ? 'Yes' : 'No'}</Text>
         <Text style={styles.amenitiesText}><Text style={styles.amenity}>Men's stalls: </Text>{stallsMen}</Text>
         <Text style={styles.amenitiesText}><Text style={styles.amenity}>Urinals: </Text>{urinals}</Text>
         <Text style={styles.amenitiesText}><Text style={styles.amenity}>Women's stalls: </Text>{stallsWomen}</Text>
-        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Changing Table: </Text>{changingTable ? 'Yes': 'No'}</Text>
+        <Text style={styles.amenitiesText}><Text style={styles.amenity}>Changing Table: </Text>{changingTable ? 'Yes' : 'No'}</Text>
         <Text style={styles.amenitiesText}><Text style={styles.amenity}>Need to pay: </Text>{needBuy ? 'Yes' : 'No'}</Text>
       </View>
     </View>
@@ -44,6 +55,31 @@ const styles = StyleSheet.create({
   image: {
     width: 400,
     height: 400
+  },
+  wrapper: {
+    // marginTop: -65
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 400
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 400
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
 })
 
